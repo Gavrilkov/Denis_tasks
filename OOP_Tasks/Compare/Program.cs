8bc272d223ae2bbf6a6fc553858b397c99a77329
DESKTOP-V50OBM3\37529 <Denis Gavrilkov>
2022-08-07 17:37:27 +0300

    Changed this project in homework compare object

--- OOP_Tasks/Compare/Program.cs
+++ OOP_Tasks/Compare/Program.cs
@@ -6,7 +6,33 @@ namespace Compare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animal kot = new Animal(3);
+            Animal dog = new Animal(3);
+
+            string A = "hey";
+            string B = "hey";
+
+            if (object.ReferenceEquals(kot, dog))
+                Console.WriteLine("Same objects");
+            else
+                Console.WriteLine("Not the same objects");
+
+            Console.ReadLine();
+
+
+            if (object.ReferenceEquals(A, B))
+                Console.WriteLine("Same objects");
+            else
+                Console.WriteLine("Not the same objects");
+        }
+    }
+
+
+    public class Animal
+    {
+        public Animal(int a)
+        {
+            int age = a;
         }
     }
 }
