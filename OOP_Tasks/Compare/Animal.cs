e25e8b95975692faea24701267aaba69066c90ae
DESKTOP-V50OBM3\37529 <Denis Gavrilkov>
2022-08-10 18:28:45 +0300

    This homework compare ojects

--- OOP_Tasks/Compare/Animal.cs
+++ OOP_Tasks/Compare/Animal.cs
@@ -2,11 +2,19 @@
 {
     class Animal
     {
-        
-        public Animal(int a)
+        int age;
+        int weight;
+        public bool Equals(Animal obj)
         {
-           
+            if (this.age == obj.age && this.weight == obj.weight)
+                return true;
+            else return false;
+        }
 
+        public Animal(int a, int b)
+        {
+            age = a;
+            weight = b;
         }
     }
 }
--- OOP_Tasks/Compare/Program.cs
+++ OOP_Tasks/Compare/Program.cs
@@ -6,24 +6,18 @@ namespace Compare
     {
         static void Main(string[] args)
         {
-            Animal kot = new Animal(3);
-            Animal dog = new Animal(3);
-
-            string A = "hey";
-            string B = "hey";
-
-            if (object.ReferenceEquals(kot, dog))
-                Console.WriteLine("Same objects");
-            else
-                Console.WriteLine("Not the same objects");
-
+            Animal kot = new Animal(3,2);
+            Animal dog = new Animal(3,1);
+            bool n = kot.Equals(dog);
+            Console.WriteLine(n);
+            
             Console.ReadLine();
+            ////if (object.ReferenceEquals(kot, dog))
+            ////    Console.WriteLine("Same objects");
+            ////else
+            ////    Console.WriteLine("Not the same objects");
 
 
-            if (object.ReferenceEquals(A, B))
-                Console.WriteLine("Same objects");
-            else
-                Console.WriteLine("Not the same objects");
         }
     }
 }
