7b9f5c3e474185fa524db2bd927ba6f38ff02e69
DESKTOP-V50OBM3\37529 <Denis Gavrilkov>
2022-08-15 18:42:48 +0300

    operator overloading

--- OOP_Tasks/Compare/Animal.cs
+++ OOP_Tasks/Compare/Animal.cs
@@ -4,17 +4,56 @@
     {
         int age;
         int weight;
-        public bool Equals(Animal obj)
-        {
-            if (this.age == obj.age && this.weight == obj.weight)
-                return true;
-            else return false;
-        }
-
         public Animal(int a, int b)
         {
             age = a;
             weight = b;
         }
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType())
+                return false;
+            var f = obj as Animal;
+            if (f == null) return false;
+            if (this.age == f.age && this.weight == f.weight)
+                return true;
+
+            else return false;
+        }
+
+        public static bool operator == (Animal obj1, Animal obj2)
+        {
+
+            if (obj1 == null || obj2 == null) return false;
+            if (obj2.age == obj1.age && obj2.weight == obj1.weight)
+                return true;
+
+            else return false;
+        }
+        public static bool operator !=(Animal obj1, Animal obj2)
+        {
+
+            if (obj1 == null || obj2 == null) return true;
+            if (obj2.age == obj1.age && obj2.weight == obj1.weight)
+                return false;
+
+            else return true;
+        }
+
+
     }
+
+    //public override bool Equals(object obj)
+    //{
+    //    if (obj.GetType() != this.GetType())
+    //        return false;
+    //    if (this.age == obj.age && this.weight == obj.weight)
+    //        return true;
+    //    else return false;
+    //}
+    //public bool Equals1(Animal obj)
+    //{
+    //    if (this.age == obj.age && this.weight == obj.weight)
+    //        return true;
+    //    else return false;
 }
--- OOP_Tasks/Compare/Program.cs
+++ OOP_Tasks/Compare/Program.cs
@@ -6,18 +6,22 @@ namespace Compare
     {
         static void Main(string[] args)
         {
+            
             Animal kot = new Animal(3,2);
             Animal dog = new Animal(3,1);
-            bool n = kot.Equals(dog);
-            Console.WriteLine(n);
-            
-            Console.ReadLine();
+        //    bool f = Equals(kot, dog);
+           bool n = kot.Equals(dog);
+         //   Console.WriteLine(n);
+         //   Console.WriteLine(f);
+          //  Console.ReadLine();
+            bool result = kot == dog;
+            Console.WriteLine($" перегруженный оператор {result}");
             ////if (object.ReferenceEquals(kot, dog))
             ////    Console.WriteLine("Same objects");
             ////else
             ////    Console.WriteLine("Not the same objects");
 
-
+            
         }
     }
 }
