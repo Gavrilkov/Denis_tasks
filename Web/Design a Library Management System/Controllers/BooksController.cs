5b883ee66d44e5613c3210bb29c9c89be866dfcb
DESKTOP-V50OBM3\37529 <Denis Gavrilkov>
2023-08-16 18:06:11 +0300

    Added 3 separate controllers for each resource: BooksController, MembersController, BookingsController

---
+++ Web/Design a Library Management System/Controllers/BooksController.cs
@@ -0,0 +1,70 @@
+﻿using Design_a_Library_Management_System.Model;
+using DesignLibraryManagementSystem.Models;
+using Library;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Design_a_Library_Management_System.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public BooksController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<BookDTO>>> GetBooks()
+        {
+
+            var Books = await _context.Book.ToListAsync();
+            var BookDTOList = Books.Select(x => new BookDTO
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Author = x.Author,
+                ISBN = x.ISBN
+            }).ToList();
+            return Ok(BookDTOList);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<BookDTO>> GetBook(int id)
+        {
+            var books = await _context.Book.FindAsync(id);
+
+            if (books == null)
+            {
+                return NotFound();
+            }
+            var bookDTO = new BookDTO()
+            {
+                Id = books.Id,
+                Title = books.Title,
+                Author = books.Author,
+                ISBN = books.ISBN
+            };
+            return bookDTO;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BookEntity>> PostBooks(BookDTO Books)
+        {
+            var bookEntity = new BookEntity();
+            bookEntity.Title = Books.Title;
+            bookEntity.Author = Books.Author;
+            bookEntity.ISBN = Books.ISBN;
+
+            _context.Book.Add(bookEntity);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = bookEntity.Id }, bookEntity);
+        }
+    }
+}
---
+++ Web/Design a Library Management System/Controllers/MembersController.cs
@@ -0,0 +1,64 @@
+﻿using Design_a_Library_Management_System.Model;
+using DesignLibraryManagementSystem.Models;
+using Library;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Design_a_Library_Management_System.Controllers
+{
+    [ApiController]
+    [Route("api/members")]
+    public class MembersController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public MembersController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LibraryMemberDTO>>> GetLibraryMembers()
+        {
+            var libraryMembers = await _context.LibraryMember.ToListAsync();
+            var libraryMembersDTOList = libraryMembers.Select(x => new LibraryMemberDTO()
+            {
+                MemberID = x.MemberID,
+                Name = x.Name
+            }).ToList();
+            return libraryMembersDTOList;
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<LibraryMemberDTO>> GetLibraryMember(int id)
+        {
+            var libraryMember = await _context.LibraryMember.FindAsync(id);
+
+            if (libraryMember == null)
+            {
+                return NotFound();
+            }
+            var libraryMemberDTO = new LibraryMemberDTO()
+            {
+                MemberID = libraryMember.MemberID,
+                Name = libraryMember.Name
+            };
+            return libraryMemberDTO;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<LibraryMemberEntity>> PostMembers(LibraryMemberDTO libraryMembers)
+        {
+            var libraryMemberEntity = new LibraryMemberEntity();
+            libraryMemberEntity.Name = libraryMembers.Name;
+
+            _context.LibraryMember.Add(libraryMemberEntity);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLibraryMember", new { id = libraryMemberEntity.MemberID }, libraryMemberEntity);
+        }
+
+    }
+}
